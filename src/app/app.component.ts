c8928beb0feb550a7d9af927efa251e1cbcc84b0
K. Fauteux <fauteuxk@labo20.med.umontreal.ca>
2018-07-18 10:06:29 -0400

    maj 20180718_1006

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Posts';
+
+  posts = [
+    {
+    title: "Mon premier post",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet eros augue. Etiam facilisis lacus velit, at gravida ante ultrices a. Donec tempor velit ut risus malesuada, sit amet consectetur enim laoreet. Quisque commodo tempus mattis. In hac habitasse platea dictumst. Proin facilisis lorem sed sem rutrum pulvinar. Nunc sem enim, fringilla non vulputate sit amet, pharetra eget leo. Suspendisse id pharetra ante. ",
+    loveIts: 2,
+    created_at: new Date()
+    },
+    {
+    title: "Mon deuxième post",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet eros augue. Etiam facilisis lacus velit, at gravida ante ultrices a. Donec tempor velit ut risus malesuada, sit amet consectetur enim laoreet. Quisque commodo tempus mattis. In hac habitasse platea dictumst. Proin facilisis lorem sed sem rutrum pulvinar. Nunc sem enim, fringilla non vulputate sit amet, pharetra eget leo. Suspendisse id pharetra ante. ",
+    loveIts: -1,
+    created_at: new Date()
+    },
+    {
+    title: "Encore un post",
+    content: "Aenean ac pulvinar dolor. Vestibulum luctus tortor commodo blandit pulvinar. Suspendisse efficitur lacus sit amet magna tempor finibus. Morbi molestie lobortis accumsan. Proin fringilla sapien lectus, vestibulum efficitur ex posuere a. Fusce purus nibh, imperdiet non lectus vitae, vestibulum tristique justo. Nullam velit est, tempus vitae condimentum vitae, cursus quis metus. ",
+    loveIts: 0,
+    created_at: new Date()
+    }     
+  ];
+
+
 }
+
